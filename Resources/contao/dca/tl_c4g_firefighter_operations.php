e59e12c85d788f19ea0288427a83946528961a22
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2019-08-18 13:44:33 +0200

    fixed userId default

--- Resources/contao/dca/tl_c4g_firefighter_operations.php
+++ Resources/contao/dca/tl_c4g_firefighter_operations.php
@@ -380,7 +380,7 @@ $GLOBALS['TL_DCA']['tl_c4g_firefighter_operations'] = array
             'inputType'         => 'select',
             'foreignKey'        => 'tl_user.name',
             'eval'              => array('mandatory' => true, 'includeBlankOption' => true, 'tl_class' => 'w50'),
-            'sql'               => "int(10) unsigned NOT NULL",
+            'sql'               => "int(10) unsigned",
             'relation'          => array('type' => 'hasOne', 'load' => 'lazy')
         )
     )
