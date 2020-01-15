de575111d1ed49043515e7e0cbeaddb9b87660ac
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2020-01-15 12:02:03 +0100

    changed core classes namespaces

--- Resources/contao/dca/tl_c4g_firefighter_operations.php
+++ Resources/contao/dca/tl_c4g_firefighter_operations.php
@@ -26,7 +26,7 @@ $GLOBALS['TL_DCA']['tl_c4g_firefighter_operations'] = array
         'dataContainer'     => 'Table',
         'enableVersioning'  => true,
         'onsubmit_callback'           => array(
-            array('\con4gis\CoreBundle\Resources\contao\classes\C4GAutomator', 'purgeApiCache'),
+            array('\con4gis\CoreBundle\Classes\C4GAutomator', 'purgeApiCache'),
             array('tl_c4g_firefighter_operations', 'adjustTime')
         ),
         'databaseAssisted'  => true,
